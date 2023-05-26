ffee2f381c251038589120129bdab0ab1c6d8cd2
marianitadileo <mdileo36@gmail.com>
2023-05-26 18:00:47 +0200

    add footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,10 @@
+<footer>
+    <ul>
+        <li>
+            <i class="fa-brands fa-square-facebook"></i>
+        </li>
+        <li><i class="fa-brands fa-twitter"></i></li>
+        <li><i class="fa-brands fa-square-instagram"></i></li>
+        <li><i class="fa-brands fa-square-youtube"></i></li>
+    </ul>
+</footer>
