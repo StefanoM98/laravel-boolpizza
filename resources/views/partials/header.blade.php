56c60c698211fb3bc7c27d52a7cda520303ed5a5
StefanoM98 <smartino030@gmail.com>
2023-05-31 11:44:05 +0200

    Add: Pizza admin controller

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,20 @@
-<header class="d-flex justify-content-center align-items-center">
-    <img src="{{ Vite::asset('resources/img/pizza-hut.png') }}" alt="Pizza Hut">
+<header>
+    <div class="container d-flex justify-content-between align-items-center">
+        <img src="{{ Vite::asset('resources/img/pizza-hut.png') }}" alt="Pizza Hut">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="/">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Admin</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+
 </header>
