b6b82f4251063f70f4be099247380ba304da5d9f
Fedekh <federicocet@gmail.com>
2023-05-31 12:25:53 +0200

    complete pizzas.index

--- resources/views/pizzas/index.blade.php
+++ resources/views/pizzas/index.blade.php
@@ -1,5 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>ciao</h1>
+
+    <div class="container">
+        <div class="row row-cols-sm-1 row-cols-md-2 row-cols-lg-4 ">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Titolo</th>
+                        <th scope="col">Descrizione</th>
+                        <th scope="col">Info</th>
+                    </tr>
+                </thead>
+                @foreach ($pizzas as $pizza)
+                    <tbody>
+                        <tr>
+                            <th scope="row">{{ $pizza->id }}</th>
+                            <td>{{ $pizza->taste }}</td>
+                            <td>{{ $pizza->price }}</td>
+                            <td>{{ $pizza->tipe }}</td>
+                            <td class="d-flex flex-row border-bottom-0 gap-2 ">
+                                <a class="btn btn-success" href="{{ route('pizzas.show', $pizza->id) }}"><i
+                                        class="fa-solid fa-info fa-bounce"></i> </a>
+                                <a class="btn btn-primary" href="{{ route('pizzas.edit', $pizza->id) }}"><i
+                                        class="fa-solid fa-gear fa-bounce"></i></a>
+
+                                <form action="{{ route('pizzas.destroy', $pizza->id) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger deletBtn" type="button"><i
+                                            class="fa-solid fa-trash fa-bounce"></i></button>
+                                </form>
+                            </td>
+                        </tr>
+                    </tbody>
+                @endforeach
+            </table>
+        </div>
+    </div>
 @endsection
