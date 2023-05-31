7e078e71c9997ff2384de917df39b7a43fdcc394
Fedekh <federicocet@gmail.com>
2023-05-31 17:18:24 +0200

    fix 'Sei sicuro di voler eliminare questa pizza?'

--- resources/views/partials/delete.blade.php
+++ resources/views/partials/delete.blade.php
@@ -1,6 +1,6 @@
 <div class="hide">
     <div id="confirmDelete" class="p-4">
-        <p>Sei sicuro di voler eliminare questo fumetto?</p>
+        <p>Sei sicuro di voler eliminare questa pizza?</p>
         <button id="confirmBtn" class="border-0 text-white p-2 mx-4 rounded">Conferma</button>
         <button id="cancelBtn" class="border-0 text-white  p-2 rounded">Annulla</button>
     </div>
