93c7db65351819bf2d407c06863a1f1252106975
Giadagenovesi <giada.g@outlook.it>
2023-05-31 12:44:40 +0200

    Add create page

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PizzaController::class, 'index']);
+Route::get('/', [PizzaController::class, 'index'])->name('home');
 
 Route::resource('pizzas', PizzaAdminController::class);
